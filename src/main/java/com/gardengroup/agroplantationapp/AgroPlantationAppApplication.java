4e309eaeb8fbb62d6009e1ef2db0c2140cde3fe7
Mgll3 <miguel.alvarez1@udea.edu.co>
2024-01-14 23:27:34 -0500

    GitHub Actions

---
+++ src/main/java/com/gardengroup/agroplantationapp/AgroPlantationAppApplication.java
@@ -0,0 +1,20 @@
+package com.gardengroup.agroplantationapp;
+
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroPlantationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroPlantationAppApplication.class, args);
+
+	}
+
+
+}
+
