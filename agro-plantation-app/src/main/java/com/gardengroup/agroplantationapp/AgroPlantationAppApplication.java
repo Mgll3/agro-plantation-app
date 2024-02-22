66ce144e767240a90ff7406c1a4148c8c92a744a
Mgll3 <miguel.alvarez1@udea.edu.co>
2024-02-22 11:16:53 -0500

    Se aplica Docker al proyecto

--- agro-plantation-app/src/main/java/com/gardengroup/agroplantationapp/AgroPlantationAppApplication.java
+++ agro-plantation-app/src/main/java/com/gardengroup/agroplantationapp/AgroPlantationAppApplication.java
@@ -1,21 +1,31 @@
 package com.gardengroup.agroplantationapp;
 
-
-
-
-import com.gardengroup.agroplantationapp.exceptions.OurException;
-import com.gardengroup.agroplantationapp.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgroPlantationAppApplication {
-	@Autowired
-	private UserService userService;
 	public static void main(String[] args) {
 		SpringApplication.run(AgroPlantationAppApplication.class, args);
+		/*
+		docker build -t agro-plantation:1.2 .
+		docker pull mysql:8.1
+		docker run --name mysql-standalone -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=agroplantationapp -e MYSQL_USER=dev -e MYSQL_PASSWORD=dev -p 3306:3306 -d mysql:8.1
+		docker run -d -p 8080:8080 --name agro-plantation --link mysql-standalone:mysql 2a3dea37ef6e
 
+		docker image ls
+
+		docker commit nombre_del_contenedor nombre_de_la_imagen
+		docker commit 925ec13322ac agro-plantation
+		docker commit 36b6f8129087 mysql-standalone
+
+		docker tag nombre_de_la_imagen usuario_dockerhub/nombre_del_repositorio
+		docker tag agro-plantation:1.2 mgll3/agro-plantation-app:Api-v1.2
+		docker tag mysql-standalone mgll3/agro-plantation-app:Mysql-v1.1
+		
+		docker push mgll3/agro-plantation-app:Api-v1.2
+		docker push mgll3/agro-plantation-app:Mysql-v1.1 
+		*/
 
 	}
 
