7912d90207c94bc5e687c5e5bf4ad62d6d3fefdb
Arturo Lopez <jhonas29@gmail.com>
2024-02-29 13:43:14 +0100

    Vitest implementado en el proyecto. Configurado y funcionando

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -11,10 +14,17 @@ const __dirname = dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		setupFiles: "./src/tests/setup.ts",
+		globals: true,
+		css: true,   
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./public"),
 		},
 	},
+	
 	
 });
