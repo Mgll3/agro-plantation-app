ff0b921bce26501708cfa5e29943c793e9811ace
Arturo López <jhonas29@gmail.com>
2024-03-07 23:28:18 +0100

    Creados test para el componente Button

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Button from "./Button";
 import { BrowserRouter } from "react-router-dom";
+import user from "@testing-library/user-event";
+
+user.setup();
+
+const handleClickMock = vi.fn();
 
 const actionButtonFuncionality = {
 	actionText: "Action",
-	handleClick: vi.fn()
+	handleClick: handleClickMock
 };
 
 const submitButtonFuncionality = {
@@ -18,31 +23,75 @@ const linkButtonFuncionality = {
 
 
 
-describe("All 3 types of buttons render correctly with different props", () => {
-	it("Action type button is rendered", () => {
+describe("Action type button:", () => {
+
+	beforeEach(() => {
+		handleClickMock.mockClear();
 		render(<Button buttonColor="yellow" buttonFontSize="text-base" buttonWidth="w-53" buttonPaddingY="py-2.5" buttonFuncionality={actionButtonFuncionality} />);
+	});
+
+	it("Renders correctly", () => {
 		const actionButton = screen.getByRole("button", {name: "Action"});
 		expect(actionButton).toBeInTheDocument();
 	});
 
-	it("Submit button is rendered", () => {
+	it("Calls it´s handleClick function", async () => {
+		const actionButton = screen.getByRole("button", {name: "Action"});
+
+		await user.click(actionButton);
+
+		expect(handleClickMock).toHaveBeenCalledTimes(1);
+	});
+
+	it("Calls it´s handleClick function two times when clicked two times", async () => {
+		const actionButton = screen.getByRole("button", {name: "Action"});
+
+		await user.click(actionButton);
+		await user.click(actionButton);
+
+		expect(handleClickMock).toHaveBeenCalledTimes(2);
+	});
+});
+
+
+describe("Submit button:", () => {
+
+	beforeEach(() => {
 		render(<Button buttonColor="yellow" buttonFontSize="text-base" buttonWidth="w-53" buttonPaddingY="py-2.5" buttonFuncionality={submitButtonFuncionality} />);
+	});
+
+	it("Renders correctly", () => {
 		const submitButton = screen.getByRole("button", {name: "Submit"});
 		expect(submitButton).toBeInTheDocument();
 	});
 
-	it("Link type button is rendered inside a React Router DOM link", () => {
+	it("Has the attribute 'type=submit '", async () => {
+		const submitButton = screen.getByRole("button", {name: "Submit"});
+
+		expect(submitButton).toHaveAttribute("type", "submit");
+	});
+});
+
+describe("Link type button:", () => {
+
+	beforeEach(() => {
 		render(
 			<Button buttonColor="yellow" buttonFontSize="text-base" buttonWidth="w-53" buttonPaddingY="py-2.5" buttonFuncionality={linkButtonFuncionality} />
 			, {wrapper: BrowserRouter}
 		);
+	});
+
+	it("Renders correctly", () => {
 		const linkButton = screen.getByRole("button", {name: "Link"});
 		const linkRouterDom = screen.getByRole("link", {name: "Link"});
 
 		expect(linkButton).toBeInTheDocument();
 		expect(linkRouterDom).toBeInTheDocument();
-		
+	});
+
+	it("Has the attribute 'href=/login' ", () => {
+		const linkRouterDom = screen.getByRole("link", {name: "Link"});
+
+		expect(linkRouterDom).toHaveAttribute("href", "/login");
 	});
 });
-
-describe()
