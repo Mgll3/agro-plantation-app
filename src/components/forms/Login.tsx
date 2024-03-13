c17bdf8bf757e65566f8648ef037a97e72411465
Elliot <unforriven@gmail.com>
2024-03-13 14:37:29 -0300

    Estilos funcionando

--- src/components/forms/Login.tsx
+++ src/components/forms/Login.tsx
@@ -32,9 +32,11 @@ export default function Login({ handleSubmit, handleRegisterClick, loginState }:
 				>
 
 					<div className="flex justify-center items-center gap-4 rounded-2xl text-2xl">
-						<button onClick={() => navigate("/")}><img src="images/LogoVerde.png" alt="logo"
-							className=" bg-[transparent] w-[120px] h-[160px]" />
-						</button>
+						<abbr title="Ir a la página principal">
+							<button onClick={() => navigate("/")}><img src="images/LogoVerde.png" alt="logo"
+								className=" bg-[transparent] w-[120px] h-[160px]" />
+							</button>
+						</abbr>
 					</div>
 
 					<div className="flex flex-col justify-center items-center p-[1rem_0]">
