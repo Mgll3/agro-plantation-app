3e73976d046401b736d5c0926d28ccd47fb2ed75
Arturo López <jhonas29@gmail.com>
2024-07-08 13:12:53 +0200

    Routes updated in App.tsx. Links updated in ProducerBanner and UserBanner

--- src/components/homeElements/UserBanner.tsx
+++ src/components/homeElements/UserBanner.tsx
@@ -8,27 +8,35 @@ function UserBanner() {
 	const buttonPaddingY = "py-3.5";
 	const buttonFuncionality = {
 		linkText: "Quiero ser productor",
-		linkUrl: "/producer/register",
+		linkUrl: "/user/registerProducer"
 	};
-
-
 
 	return (
 		<div className="flex justify-center w-full p-10 bg-[#F9F8F8] font-sans">
 			<div className="w-[45%]">
-				<img className="w-full"
-					src="images/banners/UserBanner.png" alt="" 
-				/>
+				<img className="w-full" src="images/banners/UserBanner.png" alt="" />
 			</div>
 
 			<div className="w-[40%]">
 				<h2>¡Sumate a nuestra comunidad como productor!</h2>
-				<p>Completa nuestro formulario para acceder a funciones exclusivas y comparte tus mejores tips de cultivo y experiencias en tu huerta</p>
+				<p>
+					Completa nuestro formulario para acceder a funciones exclusivas y comparte tus mejores tips de cultivo y
+					experiencias en tu huerta
+				</p>
 				<h3>Compartí</h3>
 				<p>Publica tus propias experiencias, tips y consejos sobre el cultivo.</p>
 				<h3>Destaca tu huerta</h3>
-				<p>Brinda información detallada sobre tus cultivos, métodos, técnicas de cuidado de plantas, recetas creativas con tus productos y mucho más.</p>
-				<Button buttonColor={buttonColor} buttonFontSize={buttonFontSize} buttonWidth={buttonWidth} buttonPaddingY={buttonPaddingY} buttonFuncionality={buttonFuncionality} />
+				<p>
+					Brinda información detallada sobre tus cultivos, métodos, técnicas de cuidado de plantas, recetas creativas
+					con tus productos y mucho más.
+				</p>
+				<Button
+					buttonColor={buttonColor}
+					buttonFontSize={buttonFontSize}
+					buttonWidth={buttonWidth}
+					buttonPaddingY={buttonPaddingY}
+					buttonFuncionality={buttonFuncionality}
+				/>
 			</div>
 		</div>
 	);
---
+++ src/pages/user/UserPublications.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header";
+
+function UserPublications() {
+	return (
+		<>
+			<div className="w-full">
+				<Header />
+			</div>
+			PUBLICATIONS!
+		</>
+	);
+}
+
+export default UserPublications;
