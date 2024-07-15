23ba8f1532cc9faddb50baa985caee87b1a33a41
Arturo López <jhonas29@gmail.com>
2024-07-15 19:46:34 +0200

    ProducerBanner component styles updated. Font added

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import { ButtonProps } from "./buttonTypes";
 
-function Button({ buttonColor, buttonFontSize, buttonWidth, buttonPaddingY, buttonFuncionality }: ButtonProps) {
+function Button({
+	buttonColor,
+	buttonFontSize,
+	buttonWidth,
+	buttonPaddingY,
+	buttonFuncionality,
+	otherStyles
+}: ButtonProps) {
 	let bgColor: string = "";
 	let textColor: string = "";
 
@@ -35,7 +42,7 @@ function Button({ buttonColor, buttonFontSize, buttonWidth, buttonPaddingY, butt
 			<button
 				type="button"
 				onClick={buttonFuncionality.handleClick}
-				className={`${buttonWidth} ${buttonPaddingY} ${buttonFontSize} ${bgColor} ${textColor} shadow-md rounded-lg font-sans cursor-pointer font-bold tracking-widest hover:bg-brandingDarkGreen transition-all hover:text-[#F6C915]`}
+				className={`${buttonWidth} ${buttonPaddingY} ${buttonFontSize} ${bgColor} ${textColor} shadow-md rounded-lg font-sans cursor-pointer font-bold tracking-widest hover:bg-brandingDarkGreen transition-all hover:text-[#F6C915] ${otherStyles}`}
 			>
 				{buttonFuncionality.actionText}
 			</button>
@@ -44,7 +51,7 @@ function Button({ buttonColor, buttonFontSize, buttonWidth, buttonPaddingY, butt
 		return (
 			<button
 				type="submit"
-				className={`${buttonWidth} ${buttonPaddingY} ${buttonFontSize} ${bgColor} ${textColor} shadow-md rounded-lg font-sans cursor-pointer font-bold hover:bg-opacity-80 transition-all`}
+				className={`${buttonWidth} ${buttonPaddingY} ${buttonFontSize} ${bgColor} ${textColor} shadow-md rounded-lg font-sans cursor-pointer font-bold hover:bg-opacity-80 transition-all ${otherStyles}`}
 			>
 				{buttonFuncionality.submitText}
 			</button>
@@ -53,7 +60,7 @@ function Button({ buttonColor, buttonFontSize, buttonWidth, buttonPaddingY, butt
 		return (
 			<Link to={buttonFuncionality.linkUrl} className={`inline-block ${buttonWidth}`}>
 				<button
-					className={`${buttonPaddingY} ${buttonFontSize} ${bgColor} ${textColor} w-[100%] shadow-md rounded-lg font-sans cursor-pointer font-bold hover:bg-brandingDarkGreen transition-all hover:text-[#F6C915]`}
+					className={`${buttonPaddingY} ${buttonFontSize} ${bgColor} ${textColor} w-[100%] shadow-md rounded-lg font-sans cursor-pointer font-bold hover:bg-brandingDarkGreen transition-all hover:text-[#F6C915] ${otherStyles}`}
 				>
 					{buttonFuncionality.linkText}
 				</button>
--- src/components/homeElements/ProducerBanner.tsx
+++ src/components/homeElements/ProducerBanner.tsx
@@ -3,11 +3,12 @@ import { ButtonColorType } from "../button/buttonTypes";
 
 function ProducerBanner() {
 	const buttonColor: ButtonColorType = "yellow";
-	const buttonFontSize = "text-base";
+	const buttonFontSize = "text-[24px]";
 	const buttonWidth = "w-[40%]";
-	const buttonPaddingY = "py-3.5";
+	const buttonPaddingY = "py-[14px]";
+	const otherStyles = "px-[4px]";
 	const buttonFuncionality = {
-		linkText: "Publicar Ahora",
+		linkText: "Publicar ahora",
 		linkUrl: "/producer/publications/createPublication"
 	};
 
@@ -19,15 +20,18 @@ function ProducerBanner() {
 
 			<div className="w-[41.6%]"></div>
 
-			<div className="absolute top-[16%] right-0 w-[53%] h-[70%] p-10 text-grey700 bg-[#F9F8F8] rounded-2xl">
-				<h2 className="text-[36.49px]">¡Compartí tus conocimientos!</h2>
-				<p className="text-[16px]">Publica sobre tu huerta o comparte tus mejores tips de jardinería.</p>
+			<div className="absolute top-[16%] right-0 flex flex-col justify-center w-[48.6%] h-[72.42%] pl-[4.76%] pr-[2.644%] text-grey700 bg-[#F9F8F8] rounded-2xl">
+				<h2 className="leading-[50.7px] text-[36.49px] tracking-[1px]">¡Compartí tus conocimientos!</h2>
+				<p className="my-[25px] text-[16px] font-openSans">
+					Publica sobre tu huerta o comparte tus mejores tips de jardinería.
+				</p>
 				<Button
 					buttonColor={buttonColor}
 					buttonFontSize={buttonFontSize}
 					buttonWidth={buttonWidth}
 					buttonPaddingY={buttonPaddingY}
 					buttonFuncionality={buttonFuncionality}
+					otherStyles={otherStyles}
 				/>
 			</div>
 		</div>
