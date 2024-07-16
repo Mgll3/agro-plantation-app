10a93809b3dae80bf3d7d6c22f5f3ce87bc59d25
Arturo López <jhonas29@gmail.com>
2024-07-16 17:44:17 +0200

    Loading component styles updated

--- src/components/modals/Loading.tsx
+++ src/components/modals/Loading.tsx
@@ -3,18 +3,15 @@ import { CircularProgress } from "@mui/material";
 function Loading() {
 	return (
 		<div className="z-[1000] fixed top-0 left-0 flex justify-center items-center w-screen bg-screenDarkening h-screen">
-			<div className="flex items-center flex-col justify-between bg-brandingLightYellow w-[30vw] h-[58vh] text-center text-black font-sans rounded-xl shadow-[0_6px_16px_#94B447] p-[2rem_2rem]">
-				<h3 className="text-size2 font-light pt-2">
-					ESPERANDO RESPUESTA <br /> DEL SERVIDOR
+			<div className="flex items-center flex-col justify-between w-[550px] h-[531px] p-[64px_32px] bg-white text-center text-black font-sans font-normal rounded-2xl">
+				<CircularProgress color="success" size={"150.82px"} />
+
+				<h3 className="mt-[64px] text-[35px] leading-tight">
+					Esperando respuesta <br />
+					del servidor
 				</h3>
 
-				<div className="w-[70px]">
-					<img src="/icons/modals/server-loading.png" />
-				</div>
-
-				<p className="text-sm">Un momento por favor...</p>
-
-				<CircularProgress color="success" size={"80px"} />
+				<p className="mt-[24px] text-[24px]">Un momento por favor</p>
 			</div>
 		</div>
 	);
