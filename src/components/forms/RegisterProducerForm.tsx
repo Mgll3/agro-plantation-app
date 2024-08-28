778a0e46597d12fbd3b02225186f9df3b8a14215
Arturo López <jhonas29@gmail.com>
2024-08-28 12:37:42 +0200

    RegisterProducer & RegisterProducerForm components responsive styles 1.0 finished

--- src/components/forms/RegisterProducerForm.tsx
+++ src/components/forms/RegisterProducerForm.tsx
@@ -77,9 +77,25 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 			<form name="registerForm" onSubmit={formik.handleSubmit} className="">
 				{/* INPUTS */}
 
-				<div className="flex justify-between mb-16">
-					<div className="relative w-[46%] p-2 border border-black border-solid rounded-md">
-						<label htmlFor="gardenName" className="absolute top-[-12px] left-[15px] px-2 text-[16px] bg-white">
+				<div
+					className="flex flex-wrap justify-between mb-[3.4rem]
+					custom-1400:flex-wrap
+					custom-1400:justify-start
+					custom-1400:gap-x-[9.6rem]
+					custom-1000:mb-[5rem] custom-1400:mb-[6.4rem] custom-1900:mb-[8rem] custom-3500:mb-[12rem]"
+				>
+					<div
+						className="relative w-[100%] p-[0.42rem] mb-[4.8rem] border border-black border-solid rounded-md
+						custom-1000:p-[0.7rem] custom-1400:p-[0.8rem]
+						custom-1000:mb-[0rem]
+						custom-600:w-[95%] custom-1000:w-[44%] custom-2500:w-[40%]"
+					>
+						<label
+							htmlFor="gardenName"
+							className="absolute top-[-9px] left-[15px] px-2 text-[1.2rem] bg-white
+							custom-600:top-[-11px] custom-1400:top-[-12px] custom-2500:top-[-17px] custom-3500:top-[-23px]
+							custom-600:text-[1.4rem] custom-1000:text-[1.5rem] custom-1400:text-[1.6rem] custom-1900:text-[1.8rem] custom-2500:text-[2.1rem] custom-3500:text-[2.7rem]"
+						>
 							Nombre de tu huerta
 						</label>
 
@@ -88,16 +104,34 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 							id="gardenName"
 							placeholder="Por ejemplo: Huerta del sol"
 							{...formik.getFieldProps("gardenName")}
-							className="outline-none  p-[8px_8px_4px] w-full placeholder-grey500"
+							className="outline-none  p-[4px_4px_2px] w-full placeholder-grey500 placeholder:text-[1.1rem]
+								custom-600:p-[6px_6px_3px] custom-1400:p-[8px_8px_4px] custom-1900:p-[12px_12px_6px] custom-3500:p-[16px_16px_10px]
+								custom-1200:text-[1.6rem] custom-1400:text-[1.8rem] custom-1900:text-[2rem] custom-2500:text-[2.3rem] custom-3500:text-[3.2rem]
+								custom-390:placeholder:text-[1.4rem] custom-600:placeholder:text-[1.5rem] custom-1400:placeholder:text-[1.6rem] custom-1900:placeholder:text-[1.8rem] custom-2500:placeholder:text-[2rem] custom-3500:placeholder:text-[2.8rem]"
 						/>
 
 						{formik.touched.gardenName && formik.errors.gardenName ? (
-							<p className="absolute bottom-[-25px] text-[14px] text-red-600 ml-4">{formik.errors.gardenName}</p>
+							<p
+								className="absolute bottom-[-25px] text-[1.3rem] text-red-600 ml-4
+								custom-2500:bottom-[-34px] custom-3500:bottom-[-40px]
+								custom-1000:text-[1.4rem] custom-1900:text-[1.6rem] custom-2500:text-[1.8rem] custom-3500:text-[2.4rem]"
+							>
+								{formik.errors.gardenName}
+							</p>
 						) : null}
 					</div>
 
-					<div className="relative w-[46%] p-2 border border-black border-solid rounded-md">
-						<label htmlFor="gardenAddress" className="absolute top-[-12px] left-[15px] px-2 text-[16px] bg-white">
+					<div
+						className="relative w-[100%] p-[0.42rem] border border-black border-solid rounded-md
+						custom-1000:p-[0.7rem] custom-1400:p-[0.8rem]
+						custom-600:w-[95%] custom-1000:w-[44%] custom-2500:w-[40%]"
+					>
+						<label
+							htmlFor="gardenAddress"
+							className="absolute top-[-9px] left-[15px] px-2 text-[1.2rem] bg-white
+							custom-600:top-[-11px] custom-1400:top-[-12px] custom-2500:top-[-17px] custom-3500:top-[-23px]
+							custom-600:text-[1.4rem] custom-1000:text-[1.5rem] custom-1400:text-[1.6rem] custom-1900:text-[1.8rem] custom-2500:text-[2.1rem] custom-3500:text-[2.7rem]"
+						>
 							Dirección de tu huerta
 						</label>
 
@@ -106,20 +140,41 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 							id="gardenAddress"
 							placeholder="Por ejemplo: Bermejo 356, Mendoza, Argentina"
 							{...formik.getFieldProps("gardenAddress")}
-							className="outline-none  p-[8px_8px_4px] w-full placeholder-grey500"
+							className="outline-none  p-[4px_4px_2px] w-full placeholder-grey500 placeholder:text-[1.1rem]
+								custom-600:p-[6px_6px_3px] custom-1400:p-[8px_8px_4px] custom-1900:p-[12px_12px_6px] custom-3500:p-[16px_16px_10px]
+								custom-1200:text-[1.6rem] custom-1400:text-[1.8rem] custom-1900:text-[2rem] custom-2500:text-[2.3rem] custom-3500:text-[3.2rem]
+								custom-390:placeholder:text-[1.4rem] custom-600:placeholder:text-[1.5rem] custom-1400:placeholder:text-[1.6rem] custom-1900:placeholder:text-[1.8rem] custom-2500:placeholder:text-[2rem] custom-3500:placeholder:text-[2.8rem]"
 						/>
 
 						{formik.touched.gardenAddress && formik.errors.gardenAddress ? (
-							<p className="absolute bottom-[-25px] text-[14px] text-red-600 ml-4">{formik.errors.gardenAddress}</p>
+							<p
+								className="absolute bottom-[-25px] text-[1.3rem] text-red-600 ml-4
+								custom-2500:bottom-[-34px] custom-3500:bottom-[-40px]
+								custom-1000:text-[1.4rem] custom-1900:text-[1.6rem] custom-2500:text-[1.8rem] custom-3500:text-[2.4rem]"
+							>
+								{formik.errors.gardenAddress}
+							</p>
 						) : null}
 					</div>
 				</div>
 
 				{/* RADIOS */}
 
-				<p className="mb-8 text-[19.78px]">Tamaño de la huerta</p>
+				<p
+					className="hidden mb-[1rem] text-[1.6rem]
+					custom-600:block
+					custom-1000:mb-[1.4rem] custom-1400:mb-[1.6rem]
+					custom-1000:text-[1.8rem] custom-1400:text-[1.978rem] custom-1900:text-[2.5rem] custom-2500:text-[3rem] custom-3500:text-[4rem]"
+				>
+					Tamaño de la huerta
+				</p>
 
-				<div className="flex items-center gap-10">
+				<div
+					className="flex flex-wrap items-center gap-x-[1.8rem] pl-[0rem] text-[1.1rem]
+					custom-420:gap-x-[3.4rem] custom-1400:gap-x-[4rem] custom-1900:gap-x-[5rem] custom-3500:gap-x-[7rem]
+					custom-500:pl-[1rem] custom-1400:pl-[1.4rem]
+					custom-390:text-[1.4rem] custom-400:text-[1.6rem] custom-1000:text-[1.7rem] custom-1400:text-[1.978rem] custom-1900:text-[2.5rem] custom-2500:text-[2.8rem] custom-3500:text-[3rem]"
+				>
 					<div className="flex">
 						<input
 							type="radio"
@@ -128,9 +183,14 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 							value="familiar"
 							onChange={formik.handleChange}
 							onBlur={formik.handleBlur}
-							className="cursor-pointer"
+							className="cursor-pointer
+								custom-1900:scale-[1.4] custom-2500:scale-[1.6] custom-3500:scale-[1.7]"
 						/>
-						<label htmlFor="familiar" className="ml-3 font-light cursor-pointer">
+						<label
+							htmlFor="familiar"
+							className="ml-[0.8rem] font-light cursor-pointer
+							custom-1400:ml-[1.4rem]"
+						>
 							Familiar
 						</label>
 					</div>
@@ -143,9 +203,14 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 							value="comunitaria"
 							onChange={formik.handleChange}
 							onBlur={formik.handleBlur}
-							className="cursor-pointer"
+							className="cursor-pointer
+								custom-1900:scale-[1.4] custom-2500:scale-[1.6] custom-3500:scale-[1.7]"
 						/>
-						<label htmlFor="comunitaria" className="ml-3 font-light cursor-pointer">
+						<label
+							htmlFor="comunitaria"
+							className="ml-[0.8rem] font-light cursor-pointer
+							custom-1400:ml-[1.4rem]"
+						>
 							Comunitaria
 						</label>
 					</div>
@@ -158,44 +223,85 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 							value="comercial"
 							onChange={formik.handleChange}
 							onBlur={formik.handleBlur}
-							className="cursor-pointer"
+							className="cursor-pointer
+								custom-1900:scale-[1.4] custom-2500:scale-[1.6] custom-3500:scale-[1.7]"
 						/>
-						<label htmlFor="comercial" className="ml-3 font-light cursor-pointer">
+						<label
+							htmlFor="comercial"
+							className="ml-[0.8rem] font-light cursor-pointer
+							custom-1400:ml-[1.4rem]"
+						>
 							Comerciales
 						</label>
 					</div>
 
 					{formik.touched.gardenSize && formik.errors.gardenSize ? (
-						<p className="text-[14px] text-red-600 ml-4">{formik.errors.gardenSize}</p>
+						<p
+							className="text-[1.3rem] text-red-600 ml-4
+						custom-1000:text-[1.4rem] custom-1900:text-[1.6rem] custom-2500:text-[1.8rem] custom-3500:text-[2.4rem]"
+						>
+							{formik.errors.gardenSize}
+						</p>
 					) : null}
 				</div>
 
-				<p className="w-[67%] mt-8 text-darkGrayText">
-					<span className="text-black text-[16]">-Huerta familiar:&nbsp;</span>
-					Produce hortalizas para consumo propio, con riego y abono orgánico.
-				</p>
+				<div
+					className="mt-[2.8rem] text-[1.4rem]
+					custom-600:text-[1.5rem] custom-1400:text-[1.6rem] custom-1900:text-[1.9rem] custom-2500:text-[2.2rem] custom-3500:text-[2.6rem]"
+				>
+					<p
+						className="w-[100%] text-darkGrayText
+					custom-1400:w-[79.5%]"
+					>
+						<span className="text-black">-Huerta familiar:&nbsp;</span>
+						Produce hortalizas para consumo propio, con riego y abono orgánico.
+					</p>
 
-				<p className="w-[67%] mt-2 text-darkGrayText">
-					<span className="text-black text-[16]">-Huerta comunitaria:&nbsp;</span>
-					Producen para consumo local en áreas protegidas o al aire libre, involucrando a muchas personas con un
-					objetivo común.
-				</p>
+					<p
+						className="w-[100%] mt-[1.2rem] text-darkGrayText
+						custom-1400:mt-[1.6rem] custom-1900:mt-[1.4rem]
+						custom-1400:w-[79.5%]"
+					>
+						<span className="text-black">-Huerta comunitaria:&nbsp;</span>
+						Producen para consumo local en áreas protegidas o al aire libre, involucrando a muchas personas con un
+						objetivo común.
+					</p>
 
-				<p className="w-[67%] mt-2 text-darkGrayText">
-					<span className="text-black text-[16]">-Huerta comerciales:&nbsp;</span>
-					Producen hortalizas en áreas extensas con labores manuales y mecánicas, empleando sistemas de riego y abonos
-					naturales o caseros.
-				</p>
+					<p
+						className="w-[100%] mt-[1.2rem] text-darkGrayText
+						custom-1400:mt-[1.6rem] custom-1900:mt-[1.4rem]
+						custom-1400:w-[79.5%]"
+					>
+						<span className="text-black">-Huerta comerciales:&nbsp;</span>
+						Producen hortalizas en áreas extensas con labores manuales y mecánicas, empleando sistemas de riego y abonos
+						naturales o caseros.
+					</p>
+				</div>
 
 				{/* TEXTAREA */}
 
-				<p className="mt-16 text-[19.78px]">
+				<p
+					className="mt-[4rem] text-[1.6rem]
+					custom-1000:mt-[3.5rem] custom-1400:mt-[6.4rem] custom-3500:mt-[9rem]
+					custom-1000:text-[1.8rem] custom-1400:text-[1.978rem] custom-1900:text-[2.5rem] custom-2500:text-[3rem] custom-3500:text-[4rem]"
+				>
 					Cuéntanos un poco de tu huerta
 					<span className="text-reddishBrown">&nbsp;*</span>
 				</p>
 
-				<div className="relative w-[70%] mt-6 p-2 border border-black border-solid rounded-md">
-					<label htmlFor="description" className="absolute top-[-12px] left-[15px] px-2 text-[16px] bg-white">
+				<div
+					className="relative w-[100%] mt-[0.8rem] p-2 border border-black border-solid rounded-2xl
+					custom-1000:mt-[1.3rem] custom-1400:mt-[1.6rem] custom-2500:mt-[2.8rem] custom-3500:mt-[3.8rem]
+					custom-1400:w-[79%]
+					custom-1400:rounded-md"
+				>
+					<label
+						htmlFor="description"
+						className="absolute hidden top-[-12px] left-[15px] px-2 text-[1.2rem] bg-white
+						custom-600:block
+						custom-600:top-[-11px] custom-1400:top-[-12px] custom-2500:top-[-17px] custom-3500:top-[-23px]
+						custom-600:text-[1.4rem] custom-1000:text-[1.5rem] custom-1400:text-[1.6rem] custom-1900:text-[1.8rem] custom-2500:text-[2.1rem] custom-3500:text-[2.7rem]"
+					>
 						Descripción
 					</label>
 
@@ -204,23 +310,39 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 						id="description"
 						placeholder="Información adicional"
 						maxLength={140}
-						rows={6}
+						rows={window.innerWidth > 700 ? 6 : 13}
 						{...formik.getFieldProps("description")}
-						className="outline-none  p-[8px_8px_4px] w-full placeholder-grey500"
+						className="outline-none  p-[4px_4px_2px] w-full placeholder-grey500 placeholder:text-[1.1rem]
+							custom-600:p-[6px_6px_3px] custom-1400:p-[8px_8px_4px] custom-1900:p-[12px_12px_6px] custom-3500:p-[16px_16px_10px]
+							custom-1200:text-[1.6rem] custom-1400:text-[1.8rem] custom-1900:text-[2rem] custom-2500:text-[2.3rem] custom-3500:text-[3.2rem]
+							custom-390:placeholder:text-[1.4rem] custom-600:placeholder:text-[1.5rem] custom-1400:placeholder:text-[1.6rem] custom-1900:placeholder:text-[1.8rem] custom-2500:placeholder:text-[2rem] custom-3500:placeholder:text-[2.8rem]"
 					/>
 
 					{formik.touched.description && formik.errors.description ? (
-						<p className="absolute bottom-[10px] left-[35%] text-[14px] text-red-600 ml-4">
+						<p
+							className="absolute bottom-[10px] text-[1.3rem] text-red-600 ml-4
+							custom-1000:text-[1.4rem] custom-1900:text-[1.6rem] custom-2500:text-[1.8rem] custom-3500:text-[2.4rem]"
+						>
 							{formik.errors.description}
 						</p>
 					) : null}
 				</div>
 
-				<p className="my-4 text-darkGrayText text-[18px]">{`${textAreaCharacters} / ${textAreaCharactersLeft}`}</p>
+				<p
+					className="mb-[1.6rem] mt-[0.6rem] text-darkGrayText text-[1.4rem]
+					custom-1900:mb-[2.6rem]
+					custom-1000:mt-[1rem] custom-1400:mt-[1.6rem]
+					custom-1000:text-[1.6rem] custom-1400:text-[1.8rem] custom-1900:text-[2rem] custom-2500:text-[2.2rem] custom-3500:text-[2.6rem]"
+				>
+					{`${textAreaCharacters} / ${textAreaCharactersLeft}`}
+				</p>
 
 				{/* CHECKBOX */}
 
-				<div className="flex">
+				<div
+					className="flex items-start
+					custom-1400:items-center"
+				>
 					<input
 						type="checkbox"
 						name="termsAccepted"
@@ -228,10 +350,17 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 						value="true"
 						onChange={formik.handleChange}
 						onBlur={formik.handleBlur}
-						className="scale-[180%]"
+						className="scale-[130%] mt-[0.5rem]
+							custom-1400:scale-[180%] custom-3500:scale-[250%]
+							custom-1400:mt-[0rem]"
 					/>
 
-					<label htmlFor="termsAccepted" className="ml-3 text-[18px] text-darkGrayText tracking-[0.38px]">
+					<label
+						htmlFor="termsAccepted"
+						className="ml-[0.9rem] text-[1.2rem] text-darkGrayText tracking-[0.38px]
+						custom-2500:ml-[1.9rem] custom-3500:ml-[3rem]
+						custom-600:text-[1.5rem] custom-1000:text-[1.6rem] custom-1400:text-[1.8rem] custom-1900:text-[2rem] custom-2500:text-[2.3rem] custom-3500:text-[2.8rem]"
+					>
 						Acepto los
 						<Link to="/termsAndConditions" target="_blank" className="text-blueLink">
 							&nbsp;Términos y condiciones&nbsp;
@@ -244,15 +373,23 @@ function RegisterProducerForm({ handleSubmit }: RegisterProducerFormProps) {
 				</div>
 
 				{formik.touched.termsAccepted && formik.errors.termsAccepted ? (
-					<p className="absolute text-[14px] text-red-600 ml-4">{formik.errors.termsAccepted}</p>
+					<p
+						className="absolute text-[1.3rem] text-red-600 ml-[2.5rem]
+						custom-1000:text-[1.4rem] custom-1900:text-[1.6rem] custom-2500:text-[1.8rem] custom-3500:text-[2.4rem]"
+					>
+						{formik.errors.termsAccepted}
+					</p>
 				) : null}
 
-				<div className="flex justify-center w-full mt-20">
+				<div
+					className="flex justify-center w-full mt-[3.2rem]
+					custom-1400:mt-[6.4rem] custom-2500:mt-[9rem] custom-3500:mt-[12rem]"
+				>
 					<Button
 						buttonColor="yellow"
-						buttonFontSize="text-[24px]"
-						buttonPaddingY="py-[16px]"
-						buttonWidth="w-[208px]"
+						buttonFontSize="text-[1.2rem] custom-500:text-[1.6rem] custom-900:text-[2rem] custom-1400:text-[2.4rem] custom-1900:text-[2.8rem] custom-2500:text-[3.3rem] custom-3500:text-[3.8rem]"
+						buttonPaddingY="py-[0.8rem] custom-500:py-[1.2rem] custom-1400:py-[1.4rem] custom-3500:py-[1.9rem]"
+						buttonWidth="w-[7.1rem] custom-500:w-[11rem] custom-900:w-[15rem] custom-1400:w-[20.8rem] custom-1900:w-[23rem] custom-2500:w-[30rem] custom-3500:w-[35rem]"
 						buttonFuncionality={submitButtonFuncionality}
 					/>
 				</div>
--- src/pages/user/RegisterProducer.tsx
+++ src/pages/user/RegisterProducer.tsx
@@ -66,26 +66,50 @@ function RegisterProducer() {
 	}
 
 	return (
-		<>
+		<div className="flex flex-col h-[100vh]">
 			<div className="w-full">
 				<Header />
 			</div>
 
-			<main className="w-full p-[5vw] font-sans">
+			<main
+				className="w-full px-[1.6rem] py-[3.2rem] font-sans
+					custom-600:px-[4rem] custom-900:px-[6rem] custom-1200:px-[7.5rem] custom-1400:px-[8.15rem] custom-1900:px-[10vw] custom-2500:px-[14vw]
+					custom-900:py-[6rem] custom-1400:py-[8.8rem] custom-2500:py-[14rem] custom-3500:py-[18rem]"
+			>
 				<button
 					type="button"
 					onClick={() => navigate("/")}
-					className="flex items-center py-[10px] px-[16px] mb-[64px] text-[16px] font-semibold border-2 border-brandingYellow rounded-md"
+					className="flex items-center py-[0.7rem] px-[1rem] mb-[3rem] text-[1.3rem] font-semibold border-2 border-brandingYellow rounded-md
+						custom-1400:mb-[6.4rem]
+						custom-640:text-[1.6rem] custom-1900:text-[2rem] custom-2500:text-[2.5rem] custom-3500:text-[3rem]
+						custom-1400:px-[1.6rem] custom-1900:px-[2rem]
+						custom-1400:py-[1rem] custom-1900:py-[1.1rem]"
 				>
-					<img alt="" src="/icons/Shape@2x.png" className="w-[10px] h-[17px] mr-[20px]" />
+					<img
+						alt=""
+						src="/icons/Shape@2x.png"
+						className="w-[10px] h-[17px] mr-[20px]
+						custom-1900:w-[17px]
+						custom-1900:h-[24px]"
+					/>
 					Volver
 				</button>
 
-				<h1 className="font-bold mb-[24px] text-[32px]">
+				<h1
+					className="font-bold mb-[2.4rem] text-[1.6rem] tracking-[0rem]
+					custom-600:text-[2rem] custom-1000:text-[2.5rem] custom-1400:text-[3.2rem] custom-1900:text-[3.8rem] custom-2500:text-[5rem] custom-3500:text-[6.5rem]
+					custom-1400:tracking-[-1.5%]"
+				>
 					¡Te damos la bienvenida a nuestra comunidad de productores agrícolas!
 				</h1>
 
-				<p className="w-[65%] mb-20 text-[20px]">
+				<p
+					className="w-[100%] mb-[3.5rem] text-[1.2rem] tracking-[0.04rem]
+					custom-1400:w-[80%]
+					custom-1400:mb-[6.4rem] custom-2500:mb-[8.5rem] custom-3500:mb-[10rem]
+					custom-600:text-[1.5rem] custom-1000:text-[1.8rem] custom-1400:text-[2rem] custom-1900:text-[2.5rem] custom-2500:text-[3rem] custom-3500:text-[4rem]
+					custom-1400:tracking-[0rem]"
+				>
 					Completa este breve formulario para acceder a diversos beneficios como dar a conocer tu huerta, publicar
 					artículos sobre tus sembrados, cosecha y mucho más.
 				</p>
@@ -93,7 +117,9 @@ function RegisterProducer() {
 				<RegisterProducerForm handleSubmit={submitForm} />
 			</main>
 
-			<Footer />
+			<div className="mt-auto">
+				<Footer />
+			</div>
 
 			{registerProducerState === "sending" && <Loading />}
 
@@ -118,7 +144,7 @@ function RegisterProducer() {
 			{registerProducerState === "errorServer" && (
 				<NetworkError buttonText="Aceptar" failedAction="registrar tu petición." handleClose={closeModalServerError} />
 			)}
-		</>
+		</div>
 	);
 }
 
