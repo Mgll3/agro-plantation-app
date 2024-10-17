b66898010ce244e06137aaf607c033c0fb37c62c
Arturo López <jhonas29@gmail.com>
2024-10-17 13:14:04 +0200

    AdminUsers content commented

--- src/pages/admin/AdminUsers.tsx
+++ src/pages/admin/AdminUsers.tsx
@@ -1,67 +1,67 @@
-import { useEffect, useRef } from "react";
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
-import LoadingSmall from "../../components/modals/LoadingSmall";
-import NetworkError from "../../components/modals/NetworkError";
-import useLoadingState from "../../hooks/useLoadingState";
-import { getStoredToken } from "../../utils/getStoredToken";
-import { getPendingProducerRequests } from "../../interfaces/users/getPendingProducerRequests";
+// import { useEffect, useRef } from "react";
+// import Footer from "../../components/footer/Footer";
+// import Header from "../../components/header/Header";
+// import LoadingSmall from "../../components/modals/LoadingSmall";
+// import NetworkError from "../../components/modals/NetworkError";
+// import useLoadingState from "../../hooks/useLoadingState";
+// import { getStoredToken } from "../../utils/getStoredToken";
+// import { getPendingProducerRequests } from "../../interfaces/users/getPendingProducerRequests";
 
-function AdminUsers() {
-	const [loadingState, changeLoadingState] = useLoadingState();
-	const axiosController = useRef<AbortController>();
+// function AdminUsers() {
+// 	const [loadingState, changeLoadingState] = useLoadingState();
+// 	const axiosController = useRef<AbortController>();
 
-	function closeErrorModal() {
-		changeLoadingState("loading");
-	}
+// 	function closeErrorModal() {
+// 		changeLoadingState("loading");
+// 	}
 
-	useEffect(() => {
-		axiosController.current = new AbortController();
-		const storedToken = getStoredToken();
+// 	useEffect(() => {
+// 		axiosController.current = new AbortController();
+// 		const storedToken = getStoredToken();
 
-		// if (loadingState === "loading" && storedToken) {
-		// 	getPendingProducerRequests(storedToken, axiosController.current)
-		// 		.then((response: AdminPublicationsFilteredType) => {
-		// 			const formattedPublications = formatByRandomPublications(response.publications);
-		// 			pagesLeft.current = response.pagination;
-		// 			setPublicationsFiltered(formattedPublications);
-		// 			changeLoadingState("loaded");
-		// 		})
-		// 		.catch(() => {
-		// 			changeLoadingState("errorServer");
-		// 		});
-		// }
+// 		if (loadingState === "loading" && storedToken) {
+// 			getPendingProducerRequests(storedToken, axiosController.current)
+// 				.then((response: AdminPublicationsFilteredType) => {
+// 					const formattedPublications = formatByRandomPublications(response.publications);
+// 					pagesLeft.current = response.pagination;
+// 					setPublicationsFiltered(formattedPublications);
+// 					changeLoadingState("loaded");
+// 				})
+// 				.catch(() => {
+// 					changeLoadingState("errorServer");
+// 				});
+// 		}
 
-		return () => {
-			axiosController.current?.abort();
-		};
-	}, []);
+// 		return () => {
+// 			axiosController.current?.abort();
+// 		};
+// 	}, []);
 
-	return (
-		<div className="flex flex-col min-h-[100vh]">
-			<div className="w-full">
-				<Header />
-			</div>
+// 	return (
+// 		<div className="flex flex-col min-h-[100vh]">
+// 			<div className="w-full">
+// 				<Header />
+// 			</div>
 
-			<main className="">
-				{loadingState === "loading" && (
-					<div className="min-h-[40vh] mt-24 text-brandingLightGreen">
-						<LoadingSmall />
-					</div>
-				)}
+// 			<main className="">
+// 				{loadingState === "loading" && (
+// 					<div className="min-h-[40vh] mt-24 text-brandingLightGreen">
+// 						<LoadingSmall />
+// 					</div>
+// 				)}
 
-				{loadingState === "loaded" && <></>}
+// 				{loadingState === "loaded" && <></>}
 
-				{loadingState === "errorServer" && (
-					<NetworkError failedAction="cargar las peticiones." buttonText="Entendido" handleClose={closeErrorModal} />
-				)}
-			</main>
+// 				{loadingState === "errorServer" && (
+// 					<NetworkError failedAction="cargar las peticiones." buttonText="Entendido" handleClose={closeErrorModal} />
+// 				)}
+// 			</main>
 
-			<div className="mt-auto">
-				<Footer />
-			</div>
-		</div>
-	);
-}
+// 			<div className="mt-auto">
+// 				<Footer />
+// 			</div>
+// 		</div>
+// 	);
+// }
 
-export default AdminUsers;
+// export default AdminUsers;
