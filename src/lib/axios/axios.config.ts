8f45882445a3c5a3740ef365b687b37f81bbce64
Arturo López <jhonas29@gmail.com>
2024-10-29 10:37:53 +0100

    baseURL changed in axios.config.ts to use online API

--- src/lib/axios/axios.config.ts
+++ src/lib/axios/axios.config.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosConfig = axios.create({
-	baseURL: "http://localhost:8080",
+	// baseURL: "http://localhost:8080",
+	baseURL: "https://www.agro-plantation-app.onrender.com",
 	timeout: 5000
 	// withCredentials: true
 });
