fabb563dc974f1cd6ef7fcb17c49f6c5dbe29a68
Arturo López <jhonas29@gmail.com>
2024-10-29 14:00:46 +0100

    axios baseURL changed

--- src/lib/axios/axios.config.ts
+++ src/lib/axios/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosConfig = axios.create({
 	// baseURL: "http://localhost:8080",
-	baseURL: "https://agro-plantation-app.onrender.co",
+	baseURL: "https://agro-plantation-app.onrender.com",
 	timeout: 5000
 	// withCredentials: true
 });
