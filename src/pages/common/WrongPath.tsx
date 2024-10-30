09b35427e0c668a66f3f5a01fcfc6c6595c6f4a9
Arturo López <jhonas29@gmail.com>
2024-10-30 14:05:37 +0100

    WrongPath component styles and HTML updated

--- src/pages/common/WrongPath.tsx
+++ src/pages/common/WrongPath.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { useNavigate } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import LoadingSmall from "../../components/modals/LoadingSmall";
 
 function WrongPath() {
 	const navigate = useNavigate();
@@ -16,14 +19,33 @@ function WrongPath() {
 	});
 
 	return (
-		<>
+		<div className="flex flex-col min-h-[100vh]">
 			<Helmet>
 				<title>Plant-In URL Incorrecta</title>
 			</Helmet>
-			<main>
-				<h1>La página indicada no existe. Redirigiendo a Home...</h1>
+			<div className="w-full">
+				<Header />
+			</div>
+
+			<main
+				className="flex flex-col items-center flex-grow w-[100%] min-h-[40vh] mt-[0.5rem] mx-auto
+				custom-500:mt-[2rem] custom-700:mt-[2.5rem] custom-900:mt-[3.5rem] custom-1400:mt-[8.8rem]"
+			>
+				<h1 className="text-center font-bold font-niramit text-black text-[2rem] leading-[4rem]">
+					¡Vaya! La página indicada no existe.
+					<br />
+					<span className="font-semibold text-brandingDarkGreen text-[1.8rem]"> Redirigiendo a Home...</span>
+				</h1>
+
+				<div className="my-[7vh]">
+					<LoadingSmall />
+				</div>
 			</main>
-		</>
+
+			<div className="mt-auto">
+				<Footer />
+			</div>
+		</div>
 	);
 }
 
