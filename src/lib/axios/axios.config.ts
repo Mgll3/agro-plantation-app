345baea4e413a375157c4d3259b1a3e2316813f6
Arturo López <jhonas29@gmail.com>
2024-11-14 13:00:09 +0100

    axios timeout changed to 6000ms

--- src/lib/axios/axios.config.ts
+++ src/lib/axios/axios.config.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosConfig = axios.create({
 	// baseURL: "http://localhost:8080",
 	baseURL: "https://agro-plantation-app.onrender.com",
-	timeout: 5000
+	timeout: 6000
 	// withCredentials: true
 });
