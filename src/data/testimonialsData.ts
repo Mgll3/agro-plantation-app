357b92c51b0d346ce9f00e5518c969d6400a4620
Arturo López <jhonas29@gmail.com>
2024-11-20 12:09:15 +0100

    3 new testimonials added to Testimonials

--- src/data/testimonialsData.ts
+++ src/data/testimonialsData.ts
@@ -1,47 +1,53 @@
 import { TestimonialsDataType } from "../components/homeElements/Testimonials";
 
-
 export const testimonialsData: TestimonialsDataType[] = [
 	{
 		header: "Mi verde lugar",
-		mainText: "Soy Bety, hace poco me registre en PLANT-IN y gracias a los consejos de muchos Plantineros, pude tener mis primeros brotes de mi pequeña huerta agroecologica, MUCHAS, MUCHAS GRACIAS!!!",
-		imageUrl: "images/testimonials/home-card_testimonios_1.png",
+		mainText:
+			"Soy Bety, hace poco me registre en PLANT-IN y gracias a los consejos de muchos Plantineros, pude tener mis primeros brotes de mi pequeña huerta agroecológica, MUCHAS, MUCHAS GRACIAS!!!",
+		imageUrl: "images/testimonials/home-card_testimonios_1.jpg",
 		imageAlt: "Mis primeros brotes"
 	},
 	{
-		header: "Hice un curso de Agroecologia",
-		mainText: "Gracias a los cursos de Agroecologia que dan los productores que estan registrados en PLANT-IN, pude armar mi huerta en el fondo de mi casa, ahora tengo verduras sanas y saludables",
-		imageUrl: "images/testimonials/home-card_testimonios_2.png",
+		header: "Hice un curso de Agroecología",
+		mainText:
+			"Gracias a los cursos de Agroecologia que dan los productores que estan registrados en PLANT-IN, pude armar mi huerta en el fondo de mi casa, ahora tengo verduras sanas y saludables",
+		imageUrl: "images/testimonials/home-card_testimonios_2.jpg",
 		imageAlt: "Huerta junto a mi ventana"
 	},
 	{
 		header: "Huerta Agroecologica LA CASITA",
-		mainText: "Gracias a la ayuda y los consejos de productores de otras provincias, pude aumentar la eficiencia de mi huerta, Gracias a PLANT-IN que nos conecta a todos los productores agroecologicos.",
-		imageUrl: "images/testimonials/home-card_testimonios_3.png",
+		mainText:
+			"Gracias a la ayuda y los consejos de productores de otras provincias, pude aumentar la eficiencia de mi huerta, Gracias a PLANT-IN que nos conecta a todos los productores agroecológicos.",
+		imageUrl: "images/testimonials/home-card_testimonios_3.jpg",
 		imageAlt: "Mi invernadero, a pleno rendimiento"
 	},
 	{
-		header: "Mi verde lugar",
-		mainText: "Soy Bety, hace poco me registre en PLANT-IN y gracias a los consejos de muchos Plantineros, pude tener mis primeros brotes de mi pequeña huerta agroecologica, MUCHAS, MUCHAS GRACIAS!!!",
-		imageUrl: "images/testimonials/home-card_testimonios_1.png",
-		imageAlt: "Mis primeros brotes"
+		header: "Huerta Esperanza",
+		mainText:
+			"Soy Andrés. En PLANT-IN encontré inspiración para comenzar un proyecto comunitario. Ahora, vecinos y amigos disfrutan juntos de nuestra huerta agroecológica. ¡Es un sueño hecho realidad!",
+		imageUrl: "images/testimonials/home-card_testimonios_4.jpg",
+		imageAlt: "Huerta comunitaria"
 	},
 	{
-		header: "Hice un curso de Agroecologia",
-		mainText: "Gracias a los cursos de Agroecologia que dan los productores que estan registrados en PLANT-IN, pude armar mi huerta en el fondo de mi casa, ahora tengo verduras sanas y saludables",
-		imageUrl: "images/testimonials/home-card_testimonios_2.png",
+		header: "La Casita Verde",
+		mainText:
+			"Hola, soy Camila. En PLANT-IN no solo aprendí a sembrar, también hice amigos que comparten mi pasión. Mi huerta está floreciendo, ¡gracias a todos!",
+		imageUrl: "images/testimonials/home-card_testimonios_5.jpg",
 		imageAlt: "Huerta junto a mi ventana"
 	},
 	{
 		header: "Huerta Agroecologica LA CASITA",
-		mainText: "Gracias a la ayuda y los consejos de productores de otras provincias, pude aumentar la eficiencia de mi huerta, Gracias a PLANT-IN que nos conecta a todos los productores agroecologicos.",
-		imageUrl: "images/testimonials/home-card_testimonios_3.png",
+		mainText:
+			"Gracias a la ayuda y los consejos de productores de otras provincias, pude aumentar la eficiencia de mi huerta, Gracias a PLANT-IN que nos conecta a todos los productores agroecológicos.",
+		imageUrl: "images/testimonials/home-card_testimonios_3.jpg",
 		imageAlt: "Mi invernadero, a pleno rendimiento"
 	},
 	{
-		header: "Mi verde lugar",
-		mainText: "Soy Bety, hace poco me registre en PLANT-IN y gracias a los consejos de muchos Plantineros, pude tener mis primeros brotes de mi pequeña huerta agroecologica, MUCHAS, MUCHAS GRACIAS!!!",
-		imageUrl: "images/testimonials/home-card_testimonios_1.png",
+		header: "El Edén de Luis",
+		mainText:
+			"Soy Luis y solo quiero decir que PLANT-IN me ayudó a transformar mi patio trasero en un verdadero edén lleno de vida. ¡Estoy muy agradecido por los tips y el apoyo!",
+		imageUrl: "images/testimonials/home-card_testimonios_6.jpg",
 		imageAlt: "Mis primeros brotes"
-	},
+	}
 ];
