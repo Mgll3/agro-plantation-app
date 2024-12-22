a97d65d05dc77c2f8e844b78e653d115ce2e4e9d
Arturo López <jhonas29@gmail.com>
2024-12-22 12:00:43 +0100

    axios base URL changed to AWS

--- src/lib/axios/axios.config.ts
+++ src/lib/axios/axios.config.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const axiosConfig = axios.create({
 	// baseURL: "http://localhost:8080",
-	baseURL: "https://agro-plantation-app.onrender.com",
+	// baseURL: "https://agro-plantation-app.onrender.com",
+	baseURL: "http://ec2-18-116-118-187.us-east-2.compute.amazonaws.com:8080",
 	timeout: 8000
 	// withCredentials: true
 });
