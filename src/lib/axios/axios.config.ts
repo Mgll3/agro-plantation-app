7a7d366143ad489cbb45f2dbbc12110729fe4878
Arturo López <jhonas29@gmail.com>
2025-01-24 10:05:28 +0100

    Changed API REST URL in axios.config.ts

--- src/lib/axios/axios.config.ts
+++ src/lib/axios/axios.config.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 export const axiosConfig = axios.create({
 	// baseURL: "http://localhost:8080",
 	// baseURL: "https://agro-plantation-app.onrender.com",
-	baseURL: "http://ec2-18-116-118-187.us-east-2.compute.amazonaws.com:8080",
+	// baseURL: "https://ec2-18-116-118-187.us-east-2.compute.amazonaws.com:8080",
+	baseURL: "https://farw687lql.execute-api.us-east-2.amazonaws.com/dev",
 	timeout: 8000
 	// withCredentials: true
 });
