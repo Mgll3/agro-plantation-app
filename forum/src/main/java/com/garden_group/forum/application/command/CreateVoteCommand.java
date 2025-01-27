00978cc252bc1f6617853b7f23655131ed98cdb3
Mgll3 <miguel.alvarez1@udea.edu.co>
2025-01-27 08:01:32 -0500

    Arreglar credentials

--- forum/src/main/java/com/garden_group/forum/application/command/CreateVoteCommand.java
+++ forum/src/main/java/com/garden_group/forum/application/command/CreateVoteCommand.java
@@ -1,5 +1,21 @@
 package com.garden_group.forum.application.command;
 
+import com.garden_group.forum.domain.entity.ForumUser;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class CreateVoteCommand {
 
+    private Long id;
+    @NotNull(message = "User ID is mandatory")
+    private Long user;
+    @NotNull(message = "Thread ID is mandatory")
+    private Long thread;
 }
